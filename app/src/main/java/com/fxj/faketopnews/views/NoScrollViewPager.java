7e33e899f79fef7924c6d860dbf0cd0710560b47
fuxianjin <fuxianjin-hj@huajiao.tv>
2018-07-06 18:30:13 +0800

    给NoScrollViewPager去掉滑动效果

--- app/src/main/java/com/fxj/faketopnews/views/NoScrollViewPager.java
+++ app/src/main/java/com/fxj/faketopnews/views/NoScrollViewPager.java
@@ -5,12 +5,14 @@ import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 
+import com.socks.library.KLog;
+
 /**
  * Created by fuxianjin-hj on 2018/7/3.
  */
 /**不能左右滑动的、不能处理Touch事件的ViewPager*/
 public class NoScrollViewPager extends ViewPager {
-
+    private final String tag=NoScrollViewPager.class.getSimpleName()+"_fxj";
     public NoScrollViewPager(Context context) {
         super(context);
     }
@@ -20,8 +22,20 @@ public class NoScrollViewPager extends ViewPager {
     }
 
     @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        KLog.i(tag,"NoScrollViewPager.dispatchTouchEvent");
+        return super.dispatchTouchEvent(ev);
+    }
+
+    @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {
-        //return false;
-        return super.onInterceptTouchEvent(ev);
+        KLog.i(tag,"NoScrollViewPager.onInterceptTouchEvent");
+        return false;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+     KLog.i(tag,"com.fxj.faketopnews.views.NoScrollViewPager.onTouchEvent");
+        return false;
     }
 }
